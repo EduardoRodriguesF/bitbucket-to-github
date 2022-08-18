e44730e7d01bc522288c1c3370f32b64149d7292
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-17 21:53:18 -0300

    feat: loop through list and clone

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
+source "./scripts/clone_from_bitbucket.sh"
+source "./scripts/change_remote.sh"
+
 rm -Rf tmp
 mkdir tmp
 
 echo "Listing repositories..."
 touch tmp/repos.txt
 ruby list_repos.rb > tmp/repos.txt
+
+pushd tmp
+  while read p; do
+    clone_from_bitbucket $p
+  done < repos.txt
+popd
