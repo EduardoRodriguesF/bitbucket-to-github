20196f9a20a8e6c9a228dff7854c8c0690406b57
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-17 23:05:36 -0300

    feat: do not migrate repository if it already exists

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 source "./scripts/clone_from_bitbucket.sh"
 source "./scripts/change_remote.sh"
 
+pwd="$(pwd)"
+
 rm -Rf tmp
 mkdir tmp
 
@@ -10,6 +12,12 @@ ruby list_repos.rb > tmp/repos.txt
 
 pushd tmp
   while read p; do
+    ruby $pwd/github_repository_exists.rb $p
+
+    if [[ $? -eq 0 ]]; then
+      continue
+    fi
+
     clone_from_bitbucket $p
   done < repos.txt
 popd
