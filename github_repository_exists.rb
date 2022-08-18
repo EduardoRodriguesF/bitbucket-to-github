cde7e9002ab4c8f3b18e7860d98b7bd22994d3da
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-17 22:33:11 -0300

    feat: github repository existance check script

---
+++ github_repository_exists.rb
@@ -0,0 +1,25 @@
+require 'httparty'
+require 'dotenv'
+require 'json'
+
+Dotenv.load('.env.local')
+
+@repository_name = ARGV[0]
+
+@headers = {
+  'Accept': 'application/vnd.github+json',
+  'Authorization': "token #{ENV['GITHUB_PERSONAL_TOKEN']}"
+}
+
+response = HTTParty.get(
+  "https://api.github.com/repos/EconverseAG/#{@repository_name}",
+  headers: @headers
+)
+
+response_body = JSON.parse(response.body)
+
+if response_body['message'] != 'Not Found'
+  puts "repository #{@repository_name} already exists!"
+else
+  exit 1
+end
