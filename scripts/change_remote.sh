8c0eff3b63f272c0e91c40b498b94c367ef356e9
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-17 21:52:53 -0300

    refactor: fragment migrate_repos_list into separate scripts files

--- scripts/change_remote.sh
+++ scripts/change_remote.sh
@@ -1,7 +1,3 @@
-clone_from_bitbucket() {
-  git clone git@bitbucket.org:econverse-ag/$1.git
-}
-
 change_remote() {
   pushd $1
     git remote remove origin
---
+++ scripts/clone_from_bitbucket.sh
@@ -0,0 +1,3 @@
+clone_from_bitbucket() {
+  git clone git@bitbucket.org:econverse-ag/$1.git
+}
